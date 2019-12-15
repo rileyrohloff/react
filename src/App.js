aaf47200217d1e46f819206abf0d08cb5ee9a8ea
Riley Rohloff <rileyrohloff@workfront.com>
2019-12-14 22:47:22 -0700

    added more reach components and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Todos from './components/Todos'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import uuid from 'uuid';
+
 
 class App extends Component {
   state = {
@@ -22,13 +28,50 @@ class App extends Component {
       }
     ]
   }
+  //toggle complete to change state.todo.completed
+  markComplete = (id) => {
+    this.setState( { todos: this.state.todos.map( todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) } );
+  }
+
+  delTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  };
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      complete: false
+    };
+
+
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
+
+
   render() {
-    console.log(this.state.todos)
       return (
+        <Router>
           <div className="App">
-              <h1>Riley's Testing App</h1>
-              <Todos todos={this.state.todos} />
+            <div className='container'>
+            <Header />
+            <Route path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}/>
+            <Route path="/about" component={About}/>
+            </div>
           </div>
+        </Router>
       );
   }
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,19 +6,39 @@ class TodoItem extends Component {
     getStyle = () => {
         return {
             padding: '10px',
+            borderBottom: '1px #ccc dotted',
             backgroundColor: this.props.todo.completed ?
-            '#2EBC19': 'none'
+            '#2EBC19': ''
         }
     }
+
+    markComplete = (e) => {
+        
+    }
     render() {
+        const { id, title } = this.props.todo;
         return (
         <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+            <input type='checkbox' onChange={this.props.markComplete.bind(this, id)}/> {"  "}
+            { title }
+            <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>Delete</button>
+            </p>
         </div>
         )
     }
 }
 
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 //Prop Types
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
